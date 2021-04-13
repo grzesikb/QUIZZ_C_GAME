f8dbc615c42111346baa3c943a9a4e58cd0ae04f
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2021-04-13 16:44:31 +0200

    headers added

--- QUIZZ_C_GAME/main.c
+++ QUIZZ_C_GAME/main.c
@@ -18,7 +18,9 @@ int main(void) {
     ALLEGRO_FONT* font = NULL;
     ALLEGRO_BITMAP* background = NULL;
     allegro_game_init(&timer, &display, &queue, &font, &background, &resolution_x, &resolution_y, &FPS);
+	
     init_menu(&timer, &display, &queue, &font, &background, &resolution_x, &resolution_y, &FPS);
+	
     al_rest(15.0);
     al_destroy_font(font);
     al_destroy_display(display);
