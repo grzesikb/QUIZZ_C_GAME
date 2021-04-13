27c1fc90351700c5ed8a3f4b469f86a6f53577e7
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2021-04-13 16:23:47 +0200

    files

--- QUIZZ_C_GAME/question_Wizard.h
+++ QUIZZ_C_GAME/question_Wizard.h
@@ -1,27 +1,20 @@
 ﻿#pragma once
 #include "STATUS.h"
 #include "Settings_file_handler.h"
+#include <string.h>
 void q_wizard(unsigned int );
 void ch_category(bool cat_was_empty)
 {
 	if(cat_was_empty)
 	{
-		char* name;
-		name = (char*)malloc(sizeof(char)*10);
-		int buffer = 0;
-		must_init_exit(name, "CH_CATEGORY NAME VARIABLE MEM ALLOCATION");
+		char name[300];
+		char c;
 		printf("ENTER NAME OF %d CATEGORY: ", noc_struct.the_number_of_categories);
-		while (((*name) = (char)getchar())!='\n')
+		while(((c)=getchar())!='\n')
 		{
-			if(!(buffer%10))
-			{
-				name = (char*)realloc(name, buffer + 10);
-				must_init_exit(name, "CH_CATEGORY NAME RELOCATION");
-				buffer++;
-			}
-			else buffer++;
+			strcat(name, &c);
 		}
-		printf("%s", name);
+		printf("\n%s", name);
 		
 	}else
 	{
@@ -40,5 +33,5 @@ void ch_category(bool cat_was_empty)
 
 void q_wizard(unsigned int nof_category)
 {
-	
+	5 4 3 
 }
